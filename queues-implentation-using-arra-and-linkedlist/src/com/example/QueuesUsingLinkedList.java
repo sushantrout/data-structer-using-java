4ade9f1e942e19ef302be531b42f37d6657a2b2e
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2023-01-26 23:46:25 +0530

    queues new implentation

---
+++ queues-implentation-using-arra-and-linkedlist/src/com/example/QueuesUsingLinkedList.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class QueuesUsingLinkedList {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ queues-implentation-using-arra-and-linkedlist/src/com/example/model/Node.java
@@ -0,0 +1,6 @@
+package com.example.model;
+
+public class Node {
+	public String data;
+	public Node next;
+}
